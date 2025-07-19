a9a9933a61352dc91ce45a243e6a72c7db274a7c
nghtz <203855160+LateNghtz@users.noreply.github.com>
2025-07-19 23:22:17 +0200

    more logic ;w;

--- src/scripts/guessingWord.js
+++ src/scripts/guessingWord.js
@@ -15,6 +15,28 @@ fetch(apiUrl)
     console.error("Error fetching word:", error);
 });
 
+function contains(guess, target) {
+  return guess.filter(letter => target.includes(letter));
+}
+
+function rightOrder(guess, target) {
+  let right = [];
+  let notInPlaceGuess = [];
+  let notInPlaceTarget = [];
+  for (let index = 0; index < guess.length; index++) {
+    if (guess[index] === target[index]) {
+      right.push(guess[index]);
+    }
+    else {
+      right.push("-")
+      notInPlaceGuess.push(guess[index]);
+      notInPlaceTarget.push(target[index]);
+    }
+    
+  }
+  console.log(contains(notInPlaceGuess, notInPlaceTarget)) //Checks which letters are not in place but are in the target word
+  return right;
+}
 
 
 
@@ -24,10 +46,13 @@ function guess(userGuess) {
     console.log("The target word hasn't been loaded yet.");
     return;
   }
-
-  if (userGuess === targetWord) {
-    console.log("Correct guess!");
-  } else {
-    console.log("Incorrect. Try again.");
-  }
+  
+  let splitGuess = userGuess.split("");
+  let contains = [];
+  
 }
+
+
+
+
+console.log("right: "+ rightOrder(["w", "s", "b", "d"], ["w", "r", "s", "d"]));
